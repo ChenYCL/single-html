13a2c8dcf4222d7721b179fa561fd2b614d7f74b
ChenYCL <aa34913@126.com>
2020-06-19 21:46:11 +0800

    doc

---
+++ README.md
@@ -0,0 +1,20 @@
+# what's this
+
+it's teamplate export a single html with webpack,nodejs, make images to base64 to show,
+and with .mview model.
+
+# rules
+
+- imgs
+  dir collect image
+
+- model
+  dir collect .mview etc
+
+# use
+
+global variable on window
+
+[filename]Model - model object eg. xxModel
+
+[filename]\_img - image object eg. show_img
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const path = require('path')
-const fs = require('fs')
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
+const path = require("path");
+const fs = require("fs");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-
-// 获取模型路径
-let modelOBJ = {}
-let modelArr = fs.readdirSync('./model')
+// get model
+let modelOBJ = {};
+let modelArr = fs.readdirSync("./model");
 modelArr.map((filename, idx) => {
     // 模型转 base64
-    var buff = fs.readFileSync(`./model/${filename}`)
-    var base64data = buff.toString('base64')
-    modelOBJ[filename.substr(0, filename.lastIndexOf('.'))] = base64data;
-    console.log(base64data)
-})
-console.log('\n模型转换完毕...\n');
-console.log(modelArr, '-----')
+    var buff = fs.readFileSync(`./model/${filename}`);
+    var base64data = buff.toString("base64");
+    modelOBJ[filename.substr(0, filename.lastIndexOf("."))] = base64data;
+    console.log(base64data);
+});
+console.log("\n模型转换完毕...\n");
 
-// 图片转base64
-let imgOBJ = {}
-let imgArr = fs.readdirSync('./imgs')
+// image -> base64
+let imgOBJ = {};
+let imgArr = fs.readdirSync("./imgs");
 imgArr.map((filename, idx) => {
     // 模型转 base64
-    var buff = fs.readFileSync(`./imgs/${filename}`)
-    var base64data = Buffer.from(buff, 'binary').toString('base64')
-    imgOBJ[filename.substr(0, filename.lastIndexOf('.'))] = base64data;
-    console.log(base64data)
-})
-console.log('\n图片转换完毕...开始打包\n');
-
+    var buff = fs.readFileSync(`./imgs/${filename}`);
+    var base64data = Buffer.from(buff, "binary").toString("base64");
+    imgOBJ[filename.substr(0, filename.lastIndexOf("."))] = base64data;
+    console.log(base64data);
+});
+console.log("\n图片转换完毕...开始打包\n");
 
 module.exports = {
-    entry: './index.js',
+    entry: "./index.js",
+    // node: {
+    //     __dirname: true
+    // },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'all.bundle.js',
+        path: path.resolve(__dirname, "./dist"),
+        filename: "all.bundle.js",
     },
-    mode: 'production',
+    mode: "production",
     module: {
         rules: [
-            {test: /\.txt$/, use: 'raw-loader'},
-            {test: /\.css$/, use: 'css-loader'},
-            {test: /\.css$/, use: 'style-loader'},
+            {test: /\.txt$/, use: "raw-loader"},
+            {test: /\.css$/, use: "css-loader", exclude: /node_modules/},
+            {test: /\.css$/, use: "style-loader", exclude: /node_modules/},
             {
                 test: /\.(png|jpg|gif,jpeg,gif,svg)$/i,
                 use: [
@@ -52,44 +53,43 @@ module.exports = {
                     },
                 ],
             }
-        ]
+        ],
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(
+        ),
         new HtmlWebpackPlugin({
-            template: './index.html',
-            title: 'MineX 教学',
+            template: "./index.html",
+            title: "it's a template",
             templateParameters: {
                 myscript: (() => {
-                    const d = fs.readFileSync('./dist/all.bundle.js', async (err, data) => {
-                        // console.log(data)
+                    const d = fs.readFileSync("./dist/all.bundle.js", async (err, data) => {
                         return data;
-                    })
+                    });
                     return d;
                 })(),
                 model_base64_data: (() => {
-                    let script = ''
+                    let script = "";
                     for (key in modelOBJ) {
-                        script += `var ${key}Model = "${modelOBJ[key]}";\n`
+                        script += `var ${key}Model = "${modelOBJ[key]}";\n`;
                     }
                     return script;
                 })(),
                 images_base64_data: (() => {
-                    let script = ''
+                    let script = "";
                     for (key in imgOBJ) {
-                        script += `var ${key}_img = "${imgOBJ[key]}";\n`
+                        script += `var ${key}_img = "${imgOBJ[key]}";\n`;
                     }
                     return script;
                 })(),
             },
-            inject: false,
             // minify: {
             //     removeComments: true,
             //     collapseWhitespace: true,
             //     collapseInlineTagWhitespace: true
             // }
-            minify: true
+            minify: true,
         }),
-
-    ]
+    ],
 };
